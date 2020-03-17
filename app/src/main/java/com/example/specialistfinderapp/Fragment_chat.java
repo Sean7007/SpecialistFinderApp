12f3c952eae34ddf7812d9da1f93a293a2e1c82c
Sean-Douglas Kabelo Hamathi <201402003@ub.ac.bw>
2020-03-17 23:32:13 +0200

    Version 3 of the Specialist-Finder Mobile App -Login and registration works -They both validate before they try to access database -They authenticate the data

---
+++ app/src/main/java/com/example/specialistfinderapp/Fragment_chat.java
@@ -0,0 +1,6 @@
+package com.example.specialistfinderapp;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_chat extends Fragment {
+}
---
+++ app/src/main/java/com/example/specialistfinderapp/Fragment_home.java
@@ -0,0 +1,6 @@
+package com.example.specialistfinderapp;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_home extends Fragment {
+}
